43c5ac77f91524083abbf013886f3a2e8dfb1f29
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-10-04 21:05:10 +0300

    fix for travis third try

--- samples/sample_tstack.cpp
+++ samples/sample_tstack.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	TStack<long long int> a(10);
-	for (long long int i = 1; i < 46555560; ++i) a.push(i * long long int(2));
+	for (long long int i = 1; i < 46555560; ++i) a.push(i * (long long int) 2);
 	TStack<long long int> b(a);
 	//for (long long i = 1; i < 46555561; ++i) b.push(i * long long(3));
 	//b=a;
