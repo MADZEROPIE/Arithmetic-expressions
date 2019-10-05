b28d455d37a2fd9cbe2570b97ce4c3dac91fa5a0
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-10-05 16:30:47 +0300

    delete trash

--- samples/sample_tstack.cpp
+++ samples/sample_tstack.cpp
@@ -6,13 +6,6 @@
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	TStack<long long int> a(10);
-	for (long long int i = 1; i < 46555560; ++i) a.push(i * (long long int) 2);
-	TStack<long long int> b(10);
-	for (long long i = 1; i < 46555561; ++i) b.push(i * long long(3));
-	b=a;
-	//for (int i = 1; i < 25; ++i) cout << b.pop() << ' ';
-
 	return 0;
 }
 
