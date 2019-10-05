294acbf744e78af16a3a054ffb859c07985e3df2
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-10-05 16:28:08 +0300

    New tests

--- samples/sample_tstack.cpp
+++ samples/sample_tstack.cpp
@@ -8,10 +8,10 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	TStack<long long int> a(10);
 	for (long long int i = 1; i < 46555560; ++i) a.push(i * (long long int) 2);
-	TStack<long long int> b(a);
-	//for (long long i = 1; i < 46555561; ++i) b.push(i * long long(3));
-	//b=a;
-	for (int i = 1; i < 25; ++i) cout << b.pop() << ' ';
+	TStack<long long int> b(10);
+	for (long long i = 1; i < 46555561; ++i) b.push(i * long long(3));
+	b=a;
+	//for (int i = 1; i < 25; ++i) cout << b.pop() << ' ';
 
 	return 0;
 }
