51b466b7d26626407416f4faad96c8c314cd7bd7
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-10-28 19:20:12 +0300

    added queue by array & tests for queue

--- include/tqueue.h
+++ include/tqueue.h
@@ -3,23 +3,10 @@
 #include "tstack.h"
 
 template<typename T>
-class TQueue {
+class TQueue_by_stack {
 protected:
 	TStack<T> st1, st2;
-public:
-	TQueue(int Size = 15) :st1(Size), st2(Size) {}
-	~TQueue() {}
-	void push(const T& Val) {
-		st1.push(Val);
-	}
-	void pop(void) {
-		shift();
-		return st2.pop();
-	}
-	T& top(void) {
-		shift();
-		return st2.top();
-	}
+
 	void shift() { //Перемещение элементов из 1 стека во второй
 		if (st2.IsEmpty()) {
 			while (!(st1.IsEmpty())) {
@@ -28,5 +15,71 @@ public:
 			}
 		}
 	}
+public:
+	TQueue_by_stack(int Size = 15) :st1(Size), st2(Size) {}
+	~TQueue_by_stack() {}
+
+	void push(const T& Val) {
+		st1.push(Val);
+	}
+
+	void pop(void) {
+		shift();
+		return st2.pop();
+	}
+
+	T& top(void) {
+		shift();
+		return st2.top();
+	}
+	
 };
 
+template<typename T>
+class TQueue {
+protected:
+	T* arr;
+	size_t head;
+	size_t tail;
+	size_t size;
+
+public:
+	TQueue(int Size=10) {
+		if (Size < 0) throw exception();
+		arr = new T[Size];
+		size = Size;
+		head =  0;
+		tail = 1;
+	}
+	~TQueue()
+	{
+		delete[] arr;
+	}
+
+	void push(const T& data) {
+		if (head == tail) {
+			T* tmp = new T[size + size / 3 + 1];
+			for (int i = 0; i < size; ++i) {
+					tmp[i] = arr[(head + i) % size];
+			}
+			delete[] arr;
+			arr = tmp;
+			head = 0; tail = size;
+			size = size + size / 3 + 1;
+		}
+		arr[(tail-1)%size] = data;
+		tail = (tail + 1) % size;
+		
+	}
+	T& top() {
+		if (IsEmpty()) throw exception();
+		return arr[head];
+	}
+	void pop() {
+		if (IsEmpty()) throw exception();
+		head = (head + 1) % size;
+	}
+	bool IsEmpty() { 
+		return (tail-head)==1|| head-tail==1; //некорректно (?) 
+	}
+};
